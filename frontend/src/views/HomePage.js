3c1737a5603b35d6d946dda6ad319a0dfeecae4c
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-08-16 16:42:46 +0300

    added card component and initial homepage view

---
+++ frontend/src/views/HomePage.js
@@ -0,0 +1,10 @@
+import styles from './HomePage.module.css'
+import Card from '../components/layout/Card'
+
+const HomePage = () => {
+  return <Card>
+    <h1>Hello world</h1>
+  </Card>
+}
+
+export default HomePage
