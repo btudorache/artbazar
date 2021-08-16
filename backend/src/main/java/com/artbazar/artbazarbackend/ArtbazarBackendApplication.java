c1fd19e3dad4ba2176c943ee0cf5831aa39123ec
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-08-16 14:10:13 +0300

    added initial backend setup and dependencies

---
+++ backend/src/main/java/com/artbazar/artbazarbackend/ArtbazarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.artbazar.artbazarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtbazarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtbazarBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/artbazar/artbazarbackend/ArtbazarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artbazar.artbazarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtbazarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
