7a52dfe8e0d2dc14860ae5fe370fe71b97d7e0c4
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-08-16 21:24:32 +0300

    added initial user rest controller

---
+++ backend/src/main/java/com/artbazar/artbazarbackend/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.artbazar.artbazarbackend.controller;
+
+import com.artbazar.artbazarbackend.entity.User;
+import com.artbazar.artbazarbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getUser(@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User newUser) {
+        return userService.saveUser(newUser);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
+}
