83e20234baf326c91670120520093424a4570f9e
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-08-24 20:24:14 +0300

    modified dashboard to fetch data and show posts status messages

--- frontend/src/components/layout/Dashboard.js
+++ frontend/src/components/layout/Dashboard.js
@@ -1,16 +1,29 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchPosts } from '../../store/postsSlice'
+
 import styles from './Dashboard.module.css'
 
 import Post from '../Post'
 
 const Dashboard = () => {
+  const {posts, status, error} = useSelector(state => state.posts)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchPosts())
+    }
+  }, [dispatch, status])
+
   return <div className={styles.mainLayout}>
     <div className={styles.actionSide}>
       <h2>Action Bar</h2>
     </div>
     <div className={styles.mainSide}>
-      <Post />
-      <Post />
-      <Post />
+      {status === "loading" && <p>Loading...</p>}
+      {status === 'failed' && <p className="errorText">{error}</p>}
+      {posts.map(postData => <Post key={postData.post.id} postData={postData} />)}
     </div>
     <div className={styles.secondarySide}>
 
