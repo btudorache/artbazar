b4160362c555d2f1698310ead66d1b6c5c128313
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-08-24 20:23:00 +0300

    modified Post to use backend fetched data

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,20 +1,19 @@
 import styles from "./Post.module.css";
 
-const Post = () => {
+const Post = ({postData}) => {
+  const post = postData.post
+
   return (
     <div className={styles.postGrid}>
-      <p className={styles.postUser}>Username</p>
-      <p className={styles.postDate}>Date</p>
+      <p className={styles.postUser}>{postData.postOwner}</p>
+      <p className={styles.postDate}>{post.createdAt}</p>
       <div className={styles.underline} />
-      <h2 className={styles.postTitle}>Title</h2>
+      <h2 className={styles.postTitle}>{post.title}</h2>
       <div className={styles.postPhoto}>
         <img className={styles.photo} src="https://lionalert.org/wp-content/uploads/2020/01/Lion-Cubs.jpg"/>
       </div>
       <p className={styles.postDescription}>
-        Description description description description description description
-        description description description description description description
-        description description description description description description
-        description description.
+        {post.description}
       </p>
     </div>
   );
