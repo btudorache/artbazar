cf7ec57eaaafdcc63643ffb740380b3d8e6f26c9
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-09-06 20:03:46 +0300

    modified Comment component to be able to go to comments owner profile

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,9 +1,22 @@
+import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
+
 import styles from "./Comment.module.css";
 
 const Comment = ({ commentData }) => {
+  const loggedUsername = useSelector(state => state.auth.username)
+  const history = useHistory()
+
+  const navigateToUserProfile = () => {
+    const finalUrl = loggedUsername === commentData.owner ? "/profile" : `/users/${commentData.owner}`
+    history.push(finalUrl)
+  }
+
   return (
     <div className={styles.commentLayout}>
-      <p className={styles.commentOwner}>{commentData.owner}</p>
+      <div className={styles.commentOwnerDiv}>
+        <p onClick={navigateToUserProfile} className={styles.commentOwner}>{commentData.owner}</p>
+      </div>
       <p className={styles.commentDate}>{new Date(commentData.createdAt).toDateString()}</p>
       <div className={styles.marginLine} />
       <p className={styles.commentText}>{commentData.text}</p>
