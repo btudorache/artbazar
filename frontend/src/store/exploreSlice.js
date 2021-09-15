5bd4f41e865254bde12b39ac7caff7bca3a0d01b
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-09-15 20:50:02 +0300

    added explore slice

---
+++ frontend/src/store/exploreSlice.js
@@ -0,0 +1,56 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [],
+  status: "idle",
+  error: null
+}
+
+export const fetchExplorePosts = createAsyncThunk(
+  "explore/fetchExplorePosts",
+  async (arg, thunkAPI) => {
+    const token = thunkAPI.getState().auth.token;
+    const response = await fetch("http://localhost:8080/api/posts/explore", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const posts = await response.json();
+      return posts;
+    } else {
+      throw new Error("Couldn't fetch posts")
+    }
+  }
+);
+
+const exploreSlice = createSlice({
+  name: "explore",
+  initialState: initialState,
+  reducers: {
+    resetExplorePosts(state) {
+      state.posts = []
+      state.status = 'idle'
+      state.error = null
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchExplorePosts.fulfilled, (state, action) => {
+        state.posts = action.payload;
+        state.status = "succeeded";
+      })
+      .addCase(fetchExplorePosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(fetchExplorePosts.pending, (state) => {
+        state.status = "loading";
+      });
+  }
+})
+
+export const { resetExplorePosts } = exploreSlice.actions
+
+export default exploreSlice.reducer
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import dashboardReducer from "./dashboardSlice";
 import profileReducer from "./profileSlice";
+import exploreReducer from './exploreSlice'
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     dashboard: dashboardReducer,
     profile: profileReducer,
+    explore: exploreReducer
   },
 });
